ea625e5b90b6fd84a96585fc1ee192770ca1cdef
CarlOfHoly <carl.smestad@gmail.com>
2019-09-30 17:05:19 +0200

    Fix FileStructure

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import classes from './Modal.module.css';
-import Aux from '../../../hoc/Aux'
+import Aux from '../../../hoc/Aux/Aux'
 import Backdrop from '../Backdrop/Backdrop'
 
 class Modal extends Component {
@@ -9,9 +9,7 @@ class Modal extends Component {
         return nextProps.show !== this.props.show
     }
 
-    componentDidUpdate() {
-        console.log("[Modal] WillUpdate")
-    }
+
 
     render () {
 
