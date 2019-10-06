a786a55b06fcb03d443d6a0237c38c9c95c5dcdd
CarlOfHoly <carl.smestad@gmail.com>
2019-10-06 11:39:42 +0200

    Add firebase in checkout

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -83,35 +83,12 @@ class BurgerBuilder extends Component {
     this.setState({ purchasing: false });
   };
 
-  purchaseContinueHandler = () => {
-    // alert('You continue');
-    // this.setState({ loading: true });
-    // const order = {
-    //   ingredients: this.state.ingredients,
-    //   price: this.state.totalPrice,
-    //   customer: {
-    //     name: "Carl Smestad",
-    //     address: {
-    //       street: "Trondheimveien 1a",
-    //       zipCode: "7300",
-    //       country: "Norway"
-    //     },
-    //     email: "cs@test.com"
-    //   },
-    //   deliveryMethod: "fastest"
-    // };
-    // axios
-    //   .post("/orders.json", order)
-    //   .then(response => {
-    //     console.log(response);
-    //     this.setState({ loading: false, purchasing: false });
-    //   })
-    //   .catch(error => this.setState({ loading: false, purchasing: false }));
+  purchaseContinueHandler = () => {// alert('You continue');
     const queryParams = [];
     for (let i in this.state.ingredients) {
       queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
     }
-    
+    queryParams.push('price=' +this.state.totalPrice.toFixed(2));    
     const queryString = queryParams.join('&');
     this.props.history.push({
       pathname: '/checkout',
