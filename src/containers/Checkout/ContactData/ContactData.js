f99a3987c2948b6f0867ced2145ee24fb50ae364
CarlOfHoly <carl.smestad@gmail.com>
2019-10-06 18:57:07 +0200

    add basic forms

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,76 +3,102 @@ import Button from "../../../components/UI/Button/Button";
 import classes from "./ContactData.module.css";
 import axios from "../../../axios-orders";
 import Spinner from "../../../components/UI/Spinner/Spinner";
-import { withRouter } from 'react-router-dom';
+import { withRouter } from "react-router-dom";
+import Input from "../../../components/UI/Input/Input";
 
 class ContactData extends Component {
   state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      zipCode: ''
+    orderForm: {
+      name: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Name'
+        },
+        value: ''
+      },
+      street: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Street'
+        },
+        value: ''
+      },
+      zipCode: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Zip Code'
+        },
+        value: ''
+      },
+      country: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Country'
+        },
+        value: ''
+      },
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'textemail',
+          placeholder: 'Your Email'
+        },
+        value: ''
+      },
+      deliveryMethod: {
+        elementType: 'select',
+        elementConfig: {
+          options: [{value: 'fastest', displayValue: 'Fastest'},
+                    {value: 'cheapest', displayValue: 'Cheapest'}]
+        },
+        value: ''
+      }
     },
     loading: false
   };
 
-  orderHandler = event => {
-    event.preventDefault();
-    console.log(this.props.ingredients);
+  // orderHandler = event => {
+  //   event.preventDefault();
+  //   console.log(this.props.ingredients);
 
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.props.ingredients,
-      price: this.props.price,
-      customer: {
-        name: "Carl Smestad",
-        address: {
-          street: "Trondheimveien 1a",
-          zipCode: "7300",
-          country: "Norway"
-        },
-        email: "cs@test.com"
-      },
-      deliveryMethod: "fastest"
-    };
-    axios
-      .post("/orders.json", order)
-      .then(response => {
-        console.log(response);
-        console.log(this.props.history);
-        this.setState({ loading: false });
-        this.props.history.push('/');
-      })
-      .catch(error => this.setState({ loading: false }));
-  };
+  //   this.setState({ loading: true });
+
+  //   const order = {
+  //     ingredients: this.props.ingredients,
+  //     price: this.props.price.
+  //   axios
+  //     .post("/orders.json", order)
+  //     .then(response => {
+  //       console.log(response);
+  //       console.log(this.props.history);
+  //       this.setState({ loading: false });
+  //       this.props.history.push('/');
+  //     })
+  //     .catch(error => this.setState({ loading: false }));
+  // };
 
   render() {
+    const formElementsArray = [];
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key]
+      });
+    }
+
     let form = (
       <form>
-        <input
-          className={classes.Input}
-          type="text"
-          name="name"
-          placeholder="Your name"
-        />
-        <input
-          className={classes.Input}
-          type="text"
-          name="email"
-          placeholder="Your email"
-        />
-        <input
-          className={classes.Input}
-          type="text"
-          name="street"
-          placeholder="Address"
-        />
-        <input
-          className={classes.Input}
-          type="text"
-          name="zipcode"
-          placeholder="Zip Code"
-        />
+        {formElementsArray.map(formElement => (
+          <Input 
+          key={formElement.id}
+          elementType={formElement.config.elementType}
+          elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}/>
+        ))}
         <Button btnType="Success" clicked={this.orderHandler}>
           ORDER NOW
         </Button>
